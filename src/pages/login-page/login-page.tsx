f6f0d5d687c2de954f2fd54b16aa293dc0f81bbb
Dmitry Lyapin <lyapindm@yandex.ru>
2025-09-27 15:20:56 +0300

    Add LoginForm to Login-page

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
 
 import { Page } from '@/shared/ui/page/page';
+import { LoginForm } from '@/features/auth/ui/login-form/login-form';
 
 export const LoginPage: FC = () => {
     return (
         <Page back={false}>
-            <h1 className='text-5xl'>Страница логина</h1>
+            <LoginForm />
         </Page>
     );
 };
